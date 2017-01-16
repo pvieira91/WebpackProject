3de7715bb89fcbea242ff960c556fa33c0f66508
Pedro Vieira <pvieira@becker.com>
2017-01-16 02:31:49 +0000

    express server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+//server routes...
+app.get('/hello', (req, res) => res.send({hi: 'there'}));
+
+if (process.env.NODE_ENV !== 'production'){
+	const webpackMiddleware = require('webpack-dev-middleware');
+	const webpack - require('webpack');
+	const webpackConfig = require('./webpack.config.js');
+	app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+	app.use(express.static('dist'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'dist/index.html'));
+	});
+}
+
+app.listen(3050, () => console.log('listening'));
